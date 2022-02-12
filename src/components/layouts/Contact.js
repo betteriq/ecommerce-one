14e954d4ea19a2ee644283ed3b76ad7c5ddf0b11
betteriq <anoushrs@gmail.com>
2022-02-12 12:02:27 -0800

    seventh-step

--- src/components/layouts/Contact.js
+++ src/components/layouts/Contact.js
@@ -1,20 +1,5 @@
 import React, { useEffect } from "react";
 const Contact = () => {
-  useEffect(() => {
-    const pop = document.querySelectorAll("button");
-    pop.forEach((btn) =>
-      btn.addEventListener("click", () => {
-        if (btn.classList == "btn") {
-          console.log("trueee");
-          btn.classList.remove("btn");
-        } else {
-          console.log("nooooooot");
-          btn.classList.add("btn");
-        }
-      })
-    );
-  });
-
   return (
     <div>
       <button className="btn">Toggle</button>
--- src/components/layouts/Navbar.js
+++ src/components/layouts/Navbar.js
@@ -1,24 +1,8 @@
 import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
 import { useSelector } from "react-redux";
-import useToggle from "../useToggle";
 
 const Navbar = () => {
-  const [values, setValue] = useToggle(true);
-  useEffect(() => {
-    const pop = document.querySelectorAll("h3");
-    pop.forEach((btn) =>
-      btn.addEventListener("click", () => {
-        if (btn.classList == "liner") {
-          console.log("trueee");
-          btn.classList.remove("liner");
-        } else {
-          console.log("nooooooot");
-          btn.classList.add("liner");
-        }
-      })
-    );
-  });
   const numbers = useSelector((state) => state.number);
   return (
     <nav>
@@ -26,14 +10,17 @@ const Navbar = () => {
         <Link to="/">Rashidi Shop</Link>
       </h3>
       <div className="nav-one">
-        <h3>
+        <h3 className="liner">
           <Link to="about">About</Link>
         </h3>
-        <h3>
+        <h3 className="liner">
           <Link to="contact">Contact</Link>
         </h3>
-        <h3>
+        <h3 className="liner">
           <Link to="Products">Products</Link>
+        </h3>
+        <h3 className="liner">
+          <Link to="about">Home</Link>
         </h3>
       </div>
       <div className="nav-two">
